fe4b1d7f2a5c644adc2d6a83772fc8af4ca745e9
gawahlb <wah23001@byui.edu>
2024-09-21 17:09:03 -0700

    renamed

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello World');});
 
-router.use('/users', require('./users'));
+//router.use('/users', require('./users'));
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
 
